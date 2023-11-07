ead79854883573110b77ffb5fac0b2df0d1a2e83
im-light <72781778+omar49511@users.noreply.github.com>
2023-11-06 21:16:56 -0800

    chore(database): :wrench: actualizar conexión a la BD y agregar tabla de usuario en Prisma

--- src/libs/prisma.js
+++ src/libs/prisma.js
@@ -1,3 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+const globalForPrisma = globalThis;
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
